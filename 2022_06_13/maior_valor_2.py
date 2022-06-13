d61e8f95df5a0eca9863b0d5ee7efaa0d69d6d84
charles-freitas <ccmfreitas@gmail.com>
2022-06-13 18:08:21 -0300

    Atualização de Repositório

---
+++ 2022_06_13/maior_valor_2.py
@@ -0,0 +1,20 @@
+'''
+Faça um programa em Python que leia n números inteiros e positivos 
+a partir do teclado, até que o usuário digite 0. 
+Ao final, informe o maior número digitado.
+'''
+
+maior = 0
+while True:
+    valor = int(input('Informe um valor inteiro positivo: '))
+
+    if valor == 0:
+        break
+    elif valor < 0:
+        print('ERRO: Informe apenas valores POSITIVOS...')
+        continue
+
+    # Descobrir o maior número
+    if (valor > maior): maior = valor
+
+print(f'O maior valor digitado foi {maior}')
