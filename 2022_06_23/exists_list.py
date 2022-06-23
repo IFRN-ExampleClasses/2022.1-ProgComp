8a45e78cd300b98e9286aca650090d0414d01bec
charles-freitas <ccmfreitas@gmail.com>
2022-06-23 17:55:41 -0300

    Atualização de Repositório

---
+++ 2022_06_23/exists_list.py
@@ -0,0 +1,12 @@
+texto = 'NATAL / RN'
+
+dados = [*texto]
+
+nome = 'Natal'
+letra = 'N'
+
+print('')
+print(nome in texto)
+print('')
+print(letra in texto)
+print('')
---
+++ 2022_06_23/exists_string.py
@@ -0,0 +1,10 @@
+texto = 'Joao Maria;80;90;70;65'
+
+nome = 'Joao'
+nota = '9'
+
+print('')
+print(nome in texto)
+print('')
+print(nota in texto)
+print('')
